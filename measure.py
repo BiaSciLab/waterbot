10cbdc6cba15ddc65a1e76c89ffd11947d5d04c0
biascilab <biascilab@gmail.com>
2017-12-26 12:06:15 -0500

    moved pump code to pump.py created measure code

--- measure.py
+++ measure.py
@@ -7,37 +7,13 @@ import RPi.GPIO as GPIO, time, os
 
 DEBUG = 1
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
-water_full = 0
-water_empty = 0
+pulses = 0
 
-def PumpOn (Ppin):
-    GPIO.setup(Ppin, GPIO.OUT)
-    GPIO.output(Ppin, GPIO.HIGH)
+def pulse_count(Ppin):
+    global pulses
+    pulses = pulses+1
+    print (pulses)
 
-def PumpOff (Ppin):
-    GPIO.setup(Ppin, GPIO.OUT)
-    GPIO.output(Ppin, GPIO.LOW)
-
-def RCtime (RCpin):
-    reading = 0
-    GPIO.setup(RCpin, GPIO.OUT)
-    GPIO.output(RCpin, GPIO.LOW)
-    time.sleep(0.1)
-
-    GPIO.setup(RCpin, GPIO.IN)
-    # This takes about 1 millisecond per loop cycle
-    while (GPIO.input(RCpin) == GPIO.LOW):
-        reading += 1
-        return reading
-
-while True:
-    water_full = RCtime(16)
-    water_empty = RCtime(12)
-    if water_full:
-        print ("water full ")
-        PumpOff (26)
-    if water_empty:
-        print ("water empty ")
-        PumpOn (26)
-    
+GPIO.add_event_detect(21, GPIO.RISING, callback=pulse_count)
---
+++ pump.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# Example for RC timing reading for Raspberry Pi
+# are not fast enough!
+
+import RPi.GPIO as GPIO, time, os
+
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+
+water_full = 0
+water_empty = 0
+
+def PumpOn (Ppin):
+    GPIO.setup(Ppin, GPIO.OUT)
+    GPIO.output(Ppin, GPIO.HIGH)
+
+def PumpOff (Ppin):
+    GPIO.setup(Ppin, GPIO.OUT)
+    GPIO.output(Ppin, GPIO.LOW)
+
+def RCtime (RCpin):
+    reading = 0
+    GPIO.setup(RCpin, GPIO.OUT)
+    GPIO.output(RCpin, GPIO.LOW)
+    time.sleep(0.1)
+
+    GPIO.setup(RCpin, GPIO.IN)
+    # This takes about 1 millisecond per loop cycle
+    while (GPIO.input(RCpin) == GPIO.LOW):
+        reading += 1
+        return reading
+
+while True:
+    water_full = RCtime(16)
+    water_empty = RCtime(12)
+    if water_full:
+        print ("water full ")
+        PumpOff (26)
+        time.sleep(.1)
+    if water_empty:
+        print ("water empty ")
+        PumpOn (26)
+        time.sleep(.1)
