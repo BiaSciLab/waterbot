8269b9095b74a29395cf1c6b30bf9503c539196b
biascilab <biascilab@gmail.com>
2017-12-30 15:05:50 -0500

    cleaned up code to use the same commands. added measure and constants to pump code. Moved GPIO setups to start of code instead of functions.

--- pump.py
+++ pump.py
@@ -1,44 +1,54 @@
 #!/usr/bin/env python
 
-# Example for RC timing reading for Raspberry Pi
-# are not fast enough!
-
+# Import libraries
 import RPi.GPIO as GPIO, time, os
 
 DEBUG = 1
-GPIO.setmode(GPIO.BCM)
 
-water_full = 0
-water_empty = 0
+# Pin 'constants'
+water_empty = 12
+water_full = 16
+flow_sensor = 21
+relay = 26
+
+#Variables
+pulses = 0
+ok_to_print = False
+
+#Set Up the Pi
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(water_empty, GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #12 is the empty sensor
+GPIO.setup(water_full,  GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #16 is the full sensor
+GPIO.setup(flow_sensor, GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #21 is the flow meter
+GPIO.setup(relay, GPIO.OUT) #26 is the relay for the motor
+
+# Functions
+def Pulse_Count(Ppin):
+    global pulses
+    pulses = pulses+1
 
 def PumpOn (Ppin):
-    GPIO.setup(Ppin, GPIO.OUT)
     GPIO.output(Ppin, GPIO.HIGH)
 
 def PumpOff (Ppin):
-    GPIO.setup(Ppin, GPIO.OUT)
     GPIO.output(Ppin, GPIO.LOW)
 
-def RCtime (RCpin):
-    reading = 0
-    GPIO.setup(RCpin, GPIO.OUT)
-    GPIO.output(RCpin, GPIO.LOW)
-    time.sleep(0.1)
+# inturrupts
+GPIO.add_event_detect(flow_sensor, GPIO.RISING, callback = Pulse_Count)
 
-    GPIO.setup(RCpin, GPIO.IN)
-    # This takes about 1 millisecond per loop cycle
-    while (GPIO.input(RCpin) == GPIO.LOW):
-        reading += 1
-        return reading
-
+# Main Code
 while True:
-    water_full = RCtime(16)
-    water_empty = RCtime(12)
-    if water_full:
-        print ("water full ")
-        PumpOff (26)
-        time.sleep(.1)
-    if water_empty:
-        print ("water empty ")
-        PumpOn (26)
-        time.sleep(.1)
+    # Water Full
+    if (GPIO.input(water_full) == True):
+        PumpOff (relay)
+        time.sleep(1)
+        # Print code here
+        print ("water full %d " % (pulses))
+        ok_to_print = False
+    # Water Empty
+    if GPIO.input(water_empty) == True):
+        print ("water empty")
+        ok_to_print = True # Tells the printer it can print when its done filling
+        pulses = 0
+        PumpOn (relay)
+        time.sleep(.01)
