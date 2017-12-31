6cf35b53bb5c54cf4f4953cbc5ea3207b211614d
biascilab <biascilab@gmail.com>
2017-12-30 19:10:34 -0500

    added printer

--- pump.py
+++ pump.py
@@ -2,6 +2,9 @@
 
 # Import libraries
 import RPi.GPIO as GPIO, time, os
+from Adafruit_Thermal import *
+
+printer = Adafruit_Thermal("/def/serial0", 19200, timeout=5)
 
 DEBUG = 1
 
@@ -10,7 +13,7 @@ water_empty = 12
 water_full = 16
 flow_sensor = 21
 relay = 26
-PulsesPer_mL = 240.000
+PulsesPer_mL = 650.000 # data sheet says 450 pulses per Liter my testing got 650
 
 #Variables
 pulses = 0
@@ -18,9 +21,9 @@ ok_to_print = False
 
 #Set Up the Pi
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(water_empty, GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #12 is the empty sensor
-GPIO.setup(water_full,  GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #16 is the full sensor
-GPIO.setup(flow_sensor, GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #21 is the flow meter
+GPIO.setup(water_empty, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) #12 is the empty sensor
+GPIO.setup(water_full,  GPIO.IN, pull_up_down = GPIO.PUD_DOWN) #16 is the full sensor
+GPIO.setup(flow_sensor, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) #21 is the flow meter
 GPIO.setup(relay, GPIO.OUT) #26 is the relay for the motor
 
 # Functions
@@ -40,15 +43,17 @@ GPIO.add_event_detect(flow_sensor, GPIO.RISING, callback = Pulse_Count)
 # Main Code
 while True:
     # Water Full
-    if (GPIO.input(water_full) == True):
+    if (GPIO.input(water_full) == True) and (ok_to_print == True):
         PumpOff (relay)
         current_time = time.ctime()
         time.sleep(1)
-        # Print code here
         mL_despensed = pulses / PulsesPer_mL
         print ("{} mL on ".format(mL_despensed) + current_time)
+        printer.println("{} mL on ".format(mL_despensed) + current_time)
+        printer.feed(1)
         ok_to_print = False
         pulses = 0
+        time.sleep(3)
     # Water Empty
     if (GPIO.input(water_empty) == True) or (GPIO.input(water_full) == False):
         mL_despensed = pulses / PulsesPer_mL
